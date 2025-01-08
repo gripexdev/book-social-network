36b649187bdb4409cdbd404215761e7422aa8560
gripexdev <cj.othman@gmail.com>
2025-01-08 02:36:09 +0100

    use inheritance and refactor the code

--- book-network-api/src/main/java/com/othmane/book/feedback/Feedback.java
+++ book-network-api/src/main/java/com/othmane/book/feedback/Feedback.java
@@ -1,41 +1,24 @@
 package com.othmane.book.feedback;
 
+import com.othmane.book.common.BaseEntity;
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Feedback {
-
-    @Id
-    @GeneratedValue
-    private Integer id;
+public class Feedback extends BaseEntity {
 
     // 1-5 stars
     private Double note;
 
     // feedback
     private String comment;
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;  // id of who created the entity
-
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy; // last user to modify the entity
 }
